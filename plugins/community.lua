c2e29e757535aa12df5c1d2b0140bfabab429cbc
Евгений Шевченко <3530385@gmail.com>
2023-09-30 00:10:03 +0300

    add nord theme, hard time and other

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,16 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.search.nvim-hlslens" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.color.transparent-nvim"},
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
